42c59ae81667d3b6271173d1f92081a0b620e23b
José Valderrama <josevalderrama18@gmail.com>
2024-01-20 20:21:51 +0100

    Initial Commit

---
+++ sutest/sutestApp.swift
@@ -0,0 +1,17 @@
+//
+//  sutestApp.swift
+//  sutest
+//
+//  Created by José Valderrama on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct sutestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
